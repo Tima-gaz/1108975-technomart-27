0251c9d90092428d39417a1dcac7f4dcd54f892c
Tima-gaz <60360610+Tima-gaz@users.noreply.github.com>
2020-03-20 20:31:55 +0300

    Доработал счетчик товаров

--- js/added.js
+++ js/added.js
@@ -1,26 +1,35 @@
-var link = document.querySelector(".buy");
+var link = document.querySelectorAll(".buy");
 var order = document.querySelector(".modal-order");
 var close1 = order.querySelector(".modal-close");
 var close2 = order.querySelector(".close");
 var buttonColor = document.querySelector(".cart-button");
-var count = document.getElementById("order-count").value;
-var countColor = document.getElementById("order-count")
+var count = document.querySelector("cart-button-count");
+var amount = count;
+var storageCount = count;
 
+ try {
+    storageCount = localStorage.getItem("cart-button");
+  } catch (err) {
+    isStorageSupport = false;
+  }
 
-  link.addEventListener("click", function (evt) {
+ if (storageCount) {
+      count = storageCount;
+    }
+
+for(var i=0;i<link.length;i++){
+  link[i].addEventListener("click", function (evt) {
     evt.preventDefault();
     order.classList.add("modal-show");
+    amount = amount + 1;   
+  document.getElementById('order-count').innerHTML = amount;
     
-    var amount = count;
-    amount = amount + 1;
-    
-  document.getElementById('order-count').value = amount;
-//    count.innerHTML = amount;
     if (amount){
       buttonColor.classList.add("cart-button-red");
-       countColor.classList.add("cart-button-red"); 
+       count.classList.add("cart-button-red"); 
     }  
-    
+     if (isStorageSupport) {
+        localStorage.setItem("count", count);}
   })
 
 
@@ -36,3 +45,5 @@ var countColor = document.getElementById("order-count")
     order.classList.remove("modal-show");
     
   });
+    
+}
