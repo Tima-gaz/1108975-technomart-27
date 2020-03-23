429a3eeb56797edd4de3e073088a8e761e3553ce
Tima-gaz <60360610+Tima-gaz@users.noreply.github.com>
2020-03-23 16:59:45 +0300

    Исправление ошибок 1

--- js/added-minimized.js
+++
@@ -1,5 +0,0 @@
-var link=document.querySelectorAll(".buy");var order=document.querySelector(".modal-order");var close1=order.querySelector(".modal-close");var close2=order.querySelector(".close");var buttonColor=document.querySelector(".cart-button");var count=document.querySelector("cart-button-count");var amount=count;var storageCount=count;try{storageCount=localStorage.getItem("cart-button")}catch(err){isStorageSupport=!1}
-if(storageCount){count=storageCount}
-for(var i=0;i<link.length;i++){link[i].addEventListener("click",function(evt){evt.preventDefault();order.classList.add("modal-show");amount=amount+1;document.getElementById('order-count').innerHTML=amount;if(amount){buttonColor.classList.add("cart-button-red");count.classList.add("cart-button-red")}
-if(isStorageSupport){localStorage.setItem("count",count)}})
-close1.addEventListener("click",function(evt){evt.preventDefault();order.classList.remove("modal-show")});close2.addEventListener("click",function(evt){evt.preventDefault();order.classList.remove("modal-show")})}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,148 @@
+var link = document.querySelector(".letter-button");
+  
+var popup = document.querySelector(".modal-letter");
+var close = popup.querySelector(".modal-close");
+  
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=user-name]");
+var email = popup.querySelector("[name=email]");
+var textarea = popup.querySelector("[name=text]");
+  
+var isStorageSupport = true;
+var storageName = "";
+var storageEmail = "";
+
+  try {
+    storageName = localStorage.getItem("user-name");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    textarea.value = "";
+      
+    if (storageName) {
+      login.value = storageName;
+      email.focus();
+        if(storageEmail){
+            email.value = storageEmail
+            textarea.focus();}
+    } else {
+        
+      login.focus();
+    }
+  });
+  
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    textarea.value = ""
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+  
+  form.addEventListener("submit", function (evt) {
+    if (!login.value || !email.value || !textarea.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+        localStorage.setItem("email", email.value);  
+      }
+    }
+  });
+  
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
+
+
+var orderLink = document.querySelectorAll(".buy");
+var order = document.querySelector(".modal-order");
+var close1 = order.querySelector(".modal-close");
+var close2 = order.querySelector(".close");
+var buttonColor = document.querySelector(".cart-button");
+var count = document.querySelector("cart-button-count");
+var amount = count;
+var storageCount = count;
+
+ try {
+    storageCount = localStorage.getItem("cart-button");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+ if (storageCount) {
+      count = storageCount;
+    }
+
+for(var i=0;i<orderLink.length;i++){
+  orderLink[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    order.classList.add("modal-show");
+    amount = amount + 1;   
+  document.getElementById('order-count').innerHTML = amount;
+    
+    if (amount){
+      buttonColor.classList.add("cart-button-red");
+       count.classList.add("cart-button-red"); 
+    }  
+     if (isStorageSupport) {
+        localStorage.setItem("count", count);}
+  })
+
+
+
+  close1.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    order.classList.remove("modal-show");
+    
+  });
+
+ close2.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    order.classList.remove("modal-show");
+    
+  });
+    
+}
+
+
+ var mapLink = document.querySelector(".google-map");
+
+  var mapPopup = document.querySelector(".modal-map");
+  var mapClose = mapPopup.querySelector(".modal-close");
+  
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
+
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show")) {
+        evt.preventDefault();
+        mapPopup.classList.remove("modal-show");
+      }
+    }
+  });
+
 var delivery = document.querySelector("[for=delivery]");
 var warranty = document.querySelector("[for=warranty]");
 var credit = document.querySelector("[for=credit]");
@@ -116,5 +261,3 @@ if (creditInput.hasAttribute('checked')){
     
 };
   });
-
-
